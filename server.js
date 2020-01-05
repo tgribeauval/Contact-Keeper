634a9821a48ec22e1a8d228ceb0d3331eee54ad7
Thierry Gribeauval <thierrygribeauval@Thierrys-MacBook-Air.local>
2020-01-04 21:12:21 -0500

    Connected MongoDB to Application and express.js User route.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-//Backend entry point
+//Backend entry point, defining routes
 
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Connect Database
+connectDB();
+
+//Init Middleware
+
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.json({ msg: "Welcome to the crypto abysse" }));
 
