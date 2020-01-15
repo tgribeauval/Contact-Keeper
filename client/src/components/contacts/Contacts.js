599587d969cdca69462f79e4aabb1ea3f9998854
Thierry Gribeauval <t.gribeauval11@gmail.com>
2020-01-14 21:51:24 -0500

    Debugging bugs, see you tomorrow

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,19 @@
+// import React, { Fragment, useContext } from "react";
+// import ContactContext from "../../context/contact/contactContext";
+// import ContactState from "../../context/contact/ContactState";
+
+// const Contacts = () => {
+//   const contactContext = useContext(ContactContext);
+
+//   const { contacts } = contactContext;
+
+//   return (
+//     <Fragment>
+//       {contacts.map(contact => (
+//         <h3>{contact.name}</h3>
+//       ))}
+//     </Fragment>
+//   );
+// };
+
+// export default Contacts;
