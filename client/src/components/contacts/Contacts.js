ab16b8d7d57d5e3574cb6efeafa39582cf59a94d
Thierry Gribeauval <t.gribeauval11@gmail.com>
2020-01-15 22:09:48 -0500

    Fixed bug, Contact and ContactItem Components

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,19 +1,19 @@
-// import React, { Fragment, useContext } from "react";
-// import ContactContext from "../../context/contact/contactContext";
-// import ContactState from "../../context/contact/ContactState";
+import React, { Fragment, useContext } from "react";
+import ContactContext from "../../context/contact/contactContext";
+import ContactItem from "./ContactItem";
 
-// const Contacts = () => {
-//   const contactContext = useContext(ContactContext);
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
 
-//   const { contacts } = contactContext;
+  const { contacts } = contactContext;
 
-//   return (
-//     <Fragment>
-//       {contacts.map(contact => (
-//         <h3>{contact.name}</h3>
-//       ))}
-//     </Fragment>
-//   );
-// };
+  return (
+    <Fragment>
+      {contacts.map(contact => (
+        <ContactItem key={contact.id} contact={contact} />
+      ))}
+    </Fragment>
+  );
+};
 
-// export default Contacts;
+export default Contacts;
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -8,62 +8,62 @@ import {
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
-  FILTER_CONTACT,
+  FILTER_CONTACTS,
   CLEAR_FILTER
 } from "../types";
 
-// const ContactState = props => {
-//   const initialState = {
-//     contacts: [
-//       {
-//         id: 1,
-//         name: "Thierry Gribeauval",
-//         email: "thierry.gribeauval@gmail.com",
-//         phone: "111-111-1111",
-//         type: "personal"
-//       },
-//       {
-//         id: 2,
-//         name: "Satoshi Nakamoto",
-//         email: "satoshi.nakamoto@gmail.com",
-//         phone: "222-222-2222",
-//         type: "personal"
-//       },
-//       {
-//         id: 3,
-//         name: "Vitalik Buterin",
-//         email: "vitalik.buterin@gmail.com",
-//         phone: "333-333-3333",
-//         type: "personal"
-//       }
-//     ]
-//   };
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Thierry Gribeauval",
+        email: "thierry.gribeauval@gmail.com",
+        phone: "111-111-1111",
+        type: "personal"
+      },
+      {
+        id: 2,
+        name: "Satoshi Nakamoto",
+        email: "satoshi.nakamoto@gmail.com",
+        phone: "222-222-2222",
+        type: "personal"
+      },
+      {
+        id: 3,
+        name: "Vitalik Buterin",
+        email: "vitalik.buterin@gmail.com",
+        phone: "333-333-3333",
+        type: "personal"
+      }
+    ]
+  };
 
-// const [state, dispatch] = useReducer[(contactReducer, initialState)];
+  const [state, dispatch] = useReducer(contactReducer, initialState);
 
-//Add Contact
+  //Add Contact
 
-//Delete Contact
+  //Delete Contact
 
-//Set Current Contact
+  //Set Current Contact
 
-//Clear Current Contact
+  //Clear Current Contact
 
-//Update  Contact
+  //Update  Contact
 
-//Filter Contact
+  //Filter Contact
 
-//Clear Filter
+  //Clear Filter
 
-// return (
-//   <ContactContext.Provider
-//     value={{
-//       contacts: state.contacts
-//     }}
-//   >
-//     {props.children}
-//   </ContactContext.Provider>
-// );
-// };
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
 
-// export default ContactState;
+export default ContactState;
