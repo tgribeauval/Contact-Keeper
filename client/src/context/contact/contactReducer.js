e2dab17d3521941e4fd1c75ec2a538092a7420a4
Thierry Gribeauval <t.gribeauval11@gmail.com>
2020-01-25 21:22:53 -0500

    Update contact

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -27,8 +27,9 @@ export default (state, action) => {
       };
     case UPDATE_CONTACT:
       return {
+        ...state,
         contacts: state.contacts.map(contact =>
-          contact.id === action.payload.id ? action.payload : contact
+          contact._id === action.payload._id ? action.payload : contact
         ),
         loading: false
       };
